044c2a28a7f3200033ed8263e8b47cd01db08423
semir28 <emirrahmanmulia123@gmail.com>
2024-04-02 15:39:38 +0700

    Pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Emir Rahman</li>
+ <li>Nama Belakang : Mulia</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jl. Bangka Perum Mutiara Gading Timur</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Jakarta</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ <li>Basket</li>
+ </ul>
+</section>
